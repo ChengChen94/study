09451c7cfad3a0866297fe8cd596aaba2c917bd5
chencheng94 <chencheng94@corp-ci.com>
2021-05-20 18:02:47 +0800

    vue test

---
+++ test/src/components/common/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigation"
+    }
+    new Vue({
+        data:{
+
+        },
+        props:{
+            type:Array,
+
+        }
+    })
+</script>
+
+<style scoped>
+.nav{
+    position: fixed;
+    left:0px;
+    top:0px;
+}
+.nav div{
+    flex: 1;
+}
+</style>
---
+++ test/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2 class="layui-bg-red">TEST</h2>
+    <div class="layui-icon-heart-fill"></div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
